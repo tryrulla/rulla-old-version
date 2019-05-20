dd9c5a78e73656d53bb90357f5edb6819dd13b31
Tassu <hello@tassu.me>
2019-05-20 16:11:59 +0000

    RULLA-13: User list on web
    Signed-off-by: tassu <hello@tassu.me>

---
+++ resources/js/store/userList.js
@@ -0,0 +1,36 @@
+/* eslint no-param-reassign: 0 */
+import axios from '../axios';
+
+export default {
+  namespaced: true,
+  state: {
+    loaded: false,
+    error: null,
+    data: null,
+  },
+  mutations: {
+    startLoading(state) {
+      state.data = null;
+      state.error = null;
+      state.loaded = false;
+    },
+    loaded(state, data) {
+      state.data = data;
+      state.error = null;
+      state.loaded = true;
+    },
+    loadingFailed(state, error) {
+      state.data = null;
+      state.error = error;
+      state.loaded = false;
+    },
+  },
+  actions: {
+    load({ commit, rootGetters }, { page = 1, search = '' }) {
+      const url = `${rootGetters.apiBaseUrl}/users`;
+      axios.get(url, { params: { page, search } })
+        .then(({ data }) => commit('loaded', data))
+        .catch(error => commit('loadingFailed', error));
+    },
+  },
+};
